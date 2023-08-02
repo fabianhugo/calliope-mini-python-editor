28ef738c76adfdf60531da9e5e8d4043cab9f874
Michael Malura <michael@malura.de>
2023-08-02 08:13:55 +0200

    Enable workflow dispatch, try to invalidate cloudfront distribution after deployment

--- bin/print-ci-env-stage.js
+++ bin/print-ci-env-stage.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 const ref = process.env.GITHUB_REF;
 let stage;
+let distributionId;
 
 if (ref.startsWith("refs/heads/feature")) {
   stage = "STAGING";
+  distributionId = "EJULDYM4LTZ5B";
 } else if (ref.startsWith("refs/heads/main")) {
   stage = "PRODUCTION";
+  distributionId = "E27E23HSV7YX3Y";
 } else {
   stage = "REVIEW";
 }
 
 console.log(`STAGE=${stage}`);
 console.log(`REACT_APP_STAGE=${stage}`);
+console.log(`DISTRIBUTION_ID=${distributionId}`);
